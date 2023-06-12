63d6987d2fb540fe436cb886946894a4eabccf17
Brenda <brendakajuju777@gmail.com>
2023-06-12 09:19:04 +0300

    begin rendering of payment elements

---
+++ client/payments.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    // first, fetch the publishable key from the server
+
+    fetch
+
+    // second, create the payment intent on the server 
+
+    // third, rendering the payment elements
+})
