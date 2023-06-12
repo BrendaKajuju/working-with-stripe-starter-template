e85d6cd1b94e396be063c53ad48e762a840f8558
Brenda <brendakajuju777@gmail.com>
2023-06-12 09:38:57 +0300

    complete frontend rendering of the app

--- client/payments.js
+++ client/payments.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // first, fetch the publishable key from the server
 
-    fetch
+    const {publishableKey} = await fetch('/config').then(r => r.json())
+    const stripe = Stripe(publishableKey)
 
     // second, create the payment intent on the server 
+    const {clientSecret} = await fetch ("/create-payment-intent", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(r => r.json())
 
     // third, rendering the payment elements
+    const elements = stripe.elements ({ clientSecret })
+    const paymentElement = elements.create('payment')
+    paymentElement.mount('#payment-element')
 })
